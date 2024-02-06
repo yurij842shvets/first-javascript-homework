480adae41f7e34a6e7d194629c36ea429e492391
yurij842shvets <yurij2011shvets@gmail.com>
2024-02-06 14:27:07 +0200

    add files

---
+++ script.js
@@ -0,0 +1,29 @@
+const age = 13;
+console.log(age);
+
+const name = "Yurij";
+console.log(name);
+
+const isStudent = false;
+console.log(isStudent);
+
+const myString = "The more yoyu know - the more you can";
+console.log(myString);
+
+let myNumber = 20;
+myNumber = 30;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const enterName = prompt('Enter your name');
+console.log(enterName);
+
+const confirmName = confirm('Confirm name');
+console.log(confirmName);
+
+alert('The action can be dangerous');
+
+const confirmDanger = confirm('Confirm the action');
+console.log(confirmDanger);
